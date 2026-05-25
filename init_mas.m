652478407d198b5a4feaac829918613173282941
Firas_Mahjoub <122996369+firas956@users.noreply.github.com>
2026-05-25 17:43:18 +0100

    Add files via upload
    implementation of the robot using FOC for each motor

---
+++ init_mas.m
@@ -0,0 +1,12 @@
+Rs = 1;    %Resistance statorique
+Ls = 0.25; % Inductance statorique
+Lr = Ls;   %Inductance rotorique
+Tr =0.153; %constante de temps rotorique
+sig = 0.066;%constante de dispersion
+p=2;        %nombre de pole
+J=0.07;     %Moment d'inertie du rotor
+
+Kphi=15.21;
+Kq = 1.65;
+Tphi =0.39;
+Tq = 0.0165;
